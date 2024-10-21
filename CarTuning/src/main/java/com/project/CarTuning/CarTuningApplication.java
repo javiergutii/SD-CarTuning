0b60960834d0a4fbdfc18d812f64ebced746c59b
Javier <javier.g@opendeusto.es>
2024-10-21 15:48:08 +0200

    Initial commit

---
+++ CarTuning/src/main/java/com/project/CarTuning/CarTuningApplication.java
@@ -0,0 +1,13 @@
+package com.project.CarTuning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarTuningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarTuningApplication.class, args);
+	}
+
+}
---
+++ CarTuning/src/test/java/com/project/CarTuning/CarTuningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.CarTuning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarTuningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
